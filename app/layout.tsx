84a2b8b0ee0667b930e91845dbfe120f17e48a5d
yosuke2001aoyama <yosuke2001aoyama@icloud.com>
2026-07-25 04:17:38 +0200

    Load final brand and monitoring status polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./theme.css";
 import "./osint-title.css";
+import "./ui-polish.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
